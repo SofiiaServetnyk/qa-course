c43d1c6c5bbc5a776b0929f264303395c3c044cd
Sofiia Servetnyk <sophie.villansky@gmail.com>
2023-01-26 02:38:42 +0200

    Added http response tests

---
+++ tests/api/test_http.py
@@ -0,0 +1,22 @@
+import pytest
+import requests
+
+@pytest.mark.http
+def test_first_request():
+  r = requests.get('https://api.github.com/zen')
+  print(f"The response is: {r.text}")
+
+
+@pytest.mark.http
+def test_second_request():
+    request = requests.get('https://api.github.com/users/defunkt')
+    body = request.json()
+    assert request.status_code == 200
+    assert body['name'] == 'Chris Wanstrath'
+    assert request.headers['Server'] == 'GitHub.com'
+
+
+@pytest.mark.http
+def test_status_code_request():
+  request = requests.get("https://api.github.com/users/some_user")
+  assert request.status_code == 404
